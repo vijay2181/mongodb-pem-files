9d0c5f226d5b0f7e5941dea591078ad0b8dcc879
vijay2181 <66196388+vijay2181@users.noreply.github.com>
2022-08-11 12:27:17 +0530

    Update certs.sh

--- certs.sh
+++ certs.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-CERT_SUBJECT="/C=IN/ST=TG/L=HYD/O=DEVOPS Org/OU=DEVOPS OU/CN=127.0.0.1"
+CERT_SUBJECT="/C=IN/ST=TG/L=HYD/O=DEVOPS/OU=DEV/CN=127.0.0.1/emailAddress=vijay@gmail.com"
+CN="/CN=127.0.0.1"
 
 openssl genrsa -out ca.key 2048
 openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -out ca.pem -subj "$CERT_SUBJECT"
 
 openssl genrsa -out mongodb.key 2048
-openssl req -new -key mongodb.key -out mongodb.csr -subj "$CERT_SUBJECT"
+openssl req -new -key mongodb.key -out mongodb.csr -subj "$CN"
 openssl x509 -req -in mongodb.csr -CA ca.pem -CAkey ca.key -CAcreateserial -out mongodb.crt -days 500 -sha256
 cat mongodb.key mongodb.crt > mongodb.pem
+openssl verify -CAfile ca.pem mongodb.pem
